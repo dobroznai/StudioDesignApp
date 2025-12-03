3603f999f9596b85c82fa01b06ec2e838a17064e
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-12-03 19:43:23 +0100

    first commit

---
+++ src/main/java/de/ait/training/StudioDesignAppApplication.java
@@ -0,0 +1,13 @@
+package de.ait.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioDesignAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudioDesignAppApplication.class, args);
+    }
+
+}
